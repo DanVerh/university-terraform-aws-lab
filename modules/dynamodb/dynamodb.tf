869557355d502403ce79da486b1521742395628a
DanVerh <danverh29@gmail.com>
2023-04-22 19:46:28 +0300

    comments added

--- modules/dynamodb/dynamodb.tf
+++ modules/dynamodb/dynamodb.tf
@@ -1,14 +1,17 @@
+#Table for courses
 resource "aws_dynamodb_table" "courses" {
   name         = "courses"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
 
+#We need to mention only one attribute that is a hash_key, because it is unstructured DB
   attribute {
     name = "id"
     type = "S"
   }
 }
 
+#Table for authors
 resource "aws_dynamodb_table" "authors" {
   name         = "authors"
   billing_mode = "PAY_PER_REQUEST"
--- modules/iam/all-authors.tf
+++ modules/iam/all-authors.tf
@@ -1,13 +1,16 @@
+#Create the role for get_all_authors lambda function
 resource "aws_iam_role" "get_all_authors_lambda_role" {
   name = "get-all-authors-lambda-role"
   assume_role_policy = file("./policies/lambdaRole.json")
 }
 
+#Create a policy for Dynamo DB
 resource "aws_iam_policy" "get_all_authors_dynamodb_policy" {
   name        = "get-all-authors-dynamodb-policy"
   policy      = templatefile("./policies/get-all-authors.tftpl", { authors = var.authors })
 }
 
+#Assign created policy to Lambda role
 resource "aws_iam_role_policy_attachment" "get_all_authors_dynamodb_policy_attachment" {
   policy_arn = aws_iam_policy.get_all_authors_dynamodb_policy.arn
   role       = aws_iam_role.get_all_authors_lambda_role.name
