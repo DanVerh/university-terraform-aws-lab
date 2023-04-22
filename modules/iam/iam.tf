f2259ffdc64d4300042ab5dabc3b8502948ba038
DanVerh <danverh29@gmail.com>
2023-04-22 21:48:30 +0300

    renaming done

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -1,18 +1,18 @@
 #Create the role for get_all_authors lambda function
-resource "aws_iam_role" "get_all_authors_lambda_role" {
+resource "aws_iam_role" "get_all_authors_role" {
   name = "get-all-authors-lambda-role"
   assume_role_policy = file("./policies/lambdaRole.json")
 }
 
 #Create a policy for Dynamo DB
-resource "aws_iam_policy" "get_all_authors_dynamodb_policy" {
-  name        = "get-all-authors-dynamodb-policy"
-  policy      = templatefile("./policies/dynamoDBPolicy.tftpl", { authors = var.authors })
+resource "aws_iam_policy" "get_all_authors_policy" {
+  name        = "get-all-authors-policy"
+  policy      = templatefile("./policies/authorsPolicy.tftpl", { authors = var.authors })
   #templatefile is used here to dynamically create a policy for required table that we need to take arn from
 }
 
 #Assign created policy to Lambda role
-resource "aws_iam_role_policy_attachment" "get_all_authors_dynamodb_policy_attachment" {
-  policy_arn = aws_iam_policy.get_all_authors_dynamodb_policy.arn
-  role       = aws_iam_role.get_all_authors_lambda_role.name
+resource "aws_iam_role_policy_attachment" "get_all_authors_policy_attachment" {
+  policy_arn = aws_iam_policy.get_all_authors_policy.arn
+  role       = aws_iam_role.get_all_authors_role.name
 }
