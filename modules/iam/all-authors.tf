a9c5b119ed3f4b2b71e0138d8c57f9086e5a4d0c
DanVerh <danverh29@gmail.com>
2023-04-22 19:42:33 +0300

    policies edited

--- modules/iam/all-authors.tf
+++ modules/iam/all-authors.tf
@@ -1,23 +1,11 @@
 resource "aws_iam_role" "get_all_authors_lambda_role" {
   name = "get-all-authors-lambda-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
+  assume_role_policy = file("./policies/lambdaRole.json")
 }
 
 resource "aws_iam_policy" "get_all_authors_dynamodb_policy" {
   name        = "get-all-authors-dynamodb-policy"
-  policy      = templatefile("./policies/get-all-authors.tftpl", {authors = var.authors })
+  policy      = templatefile("./policies/get-all-authors.tftpl", { authors = var.authors })
 }
 
 resource "aws_iam_role_policy_attachment" "get_all_authors_dynamodb_policy_attachment" {
