82fbcb44ded0b05c2e306480325c03a4899e1266
DanVerh <danverh29@gmail.com>
2023-05-09 16:02:30 +0300

    options method for child resource added

--- modules/api-gateway/options.tf
+++ modules/api-gateway/options.tf
@@ -1,3 +1,5 @@
+# FOR PARENT RESOURCE
+
 # Integration
 resource "aws_api_gateway_integration" "options" {
   for_each = var.courses_parent
@@ -58,3 +60,68 @@ resource "aws_api_gateway_integration_response" "options" {
 
   depends_on = [ aws_api_gateway_deployment.deployment, aws_api_gateway_method_response.options ]
 }
+
+
+# FOR CHILD RESOURCE
+
+# Integration
+resource "aws_api_gateway_integration" "options_child" {
+  for_each = var.courses_child
+
+  rest_api_id             = aws_api_gateway_rest_api.courses_api.id
+  resource_id             = aws_api_gateway_resource.courses_child.id
+  http_method             = "OPTIONS"
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = each.value.arn
+
+  depends_on = [ aws_api_gateway_method.courses_child ]
+}
+
+# Method
+resource "aws_api_gateway_method" "options_child" {
+  rest_api_id   = aws_api_gateway_rest_api.courses_api.id
+  resource_id   = aws_api_gateway_resource.courses_child.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+  api_key_required = false
+}
+
+# Response
+resource "aws_api_gateway_method_response" "options_child" {
+  rest_api_id = aws_api_gateway_rest_api.courses_api.id
+  resource_id = aws_api_gateway_resource.courses_child.id
+  http_method = "OPTIONS"
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = true,
+        "method.response.header.Access-Control-Allow-Methods" = true,
+        "method.response.header.Access-Control-Allow-Origin" = true,
+        "method.response.header.Access-Control-Expose-Headers" = true
+    }
+
+  depends_on = [ aws_api_gateway_deployment.deployment ]
+}
+
+# Integration Response
+resource "aws_api_gateway_integration_response" "options_child" {
+  rest_api_id = aws_api_gateway_rest_api.courses_api.id
+  resource_id = aws_api_gateway_resource.courses_child.id
+  http_method = "OPTIONS"
+  status_code = 200
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'*'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'",
+    "method.response.header.Access-Control-Expose-Headers" = "'*'"
+   }
+
+  depends_on = [ aws_api_gateway_deployment.deployment, aws_api_gateway_method_response.options_child ]
+}
+
