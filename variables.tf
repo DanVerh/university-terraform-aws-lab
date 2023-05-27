ebd590324b99affdce61077c99cb95f133c0ee5b
DanVerh <danverh29@gmail.com>
2023-05-28 00:03:03 +0300

    description for variables added

--- variables.tf
+++ variables.tf
@@ -1,2 +1,6 @@
-variable "email" {}
-variable "site_bucket_name" {}
+variable "email" {
+  description = "Email that will receive alarms"
+}
+variable "site_bucket_name" {
+    description = "Name of the bucket for site hosting"
+}
